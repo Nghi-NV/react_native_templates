0f3cd62b248b85aa98e958b5f591c0a3d2a51d6b
Nghi-NV <32028055+Nghi-NV@users.noreply.github.com>
2018-06-14 09:18:55 +0700

    Update app-navigator.js

--- src/app-navigator.js
+++ src/app-navigator.js
@@ -63,7 +63,7 @@ const mapDispathToProps = {
 
 const mapStateToProps = (state) => {
   return {
-    languages: state.config.language,
+    language: state.config.language,
     authen: state.authen.get('authenticated')
   }
 }
